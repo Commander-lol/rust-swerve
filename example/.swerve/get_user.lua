01e16cb0e5a671880b1080d222f79b14e7d597d6
Louis Capitanchik <contact@louiscap.co>
2018-06-19 16:18:06 +0100

    Enhance Example

--- example/.swerve/get_user.lua
+++ example/.swerve/get_user.lua
@@ -3,10 +3,18 @@ print("We're going to the races");
 foo = 1 + 1;
 
 r = empty_response();
-r:set_status(404);
--- r:set_header("foo", "bar");
--- r:set_body()
+r:set_status(200);
+r:set_header("X-Powered-By", "Swerve");
+r:set_header("Content-Type", "text/plain");
+r:set_body("This is my only response");
 
-return response(200, "application/json", json_encode({ foo = 123 }));
+--return r
+
+r = response(200, "application/json", json_encode({ foo = 123 }));
+
+r:unset_body();
+r:set_status(204);
+
+return r
 
 -- return response(200, "application/json", '{ "foo": ' .. foo .. ' }')
